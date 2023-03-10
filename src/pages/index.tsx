226064130da75ef1df494563979bae58c329d0e6
kentaro-maker <kentaro.mo.olesen@gmail.com>
2023-03-10 21:26:23 +0900

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import { Link } from 'gatsby'
 import Layout from "../layouts/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const pageStyles = {
   color: "#232129",
@@ -143,6 +144,11 @@ const IndexPage: React.FC<PageProps> = () => {
     <main style={pageStyles}>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
+
       </Layout>
 
       <h1 style={headingStyles}>
