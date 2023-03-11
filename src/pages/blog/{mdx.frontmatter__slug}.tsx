587d8c37f9ba4005e0ab87ab2f21286aec1b6aab
kentaro-maker <kentaro.mo.olesen@gmail.com>
2023-03-11 14:03:07 +0900

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react'
 import { graphql, PageProps } from 'gatsby'
+import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image'
 
 import Layout from '../../layouts/layout'
 import Seo from '../../components/seo'
 
+
 const BlogPost = ({ data, children }: PageProps<Queries.BlogPostQuery>) => {
+  const image = getImage(data.mdx?.frontmatter?.hero_image?.childImageSharp?.gatsbyImageData ?? null)
+
   return (
     <Layout pageTitle={data.mdx?.frontmatter?.title}>
-      <p>{data.mdx?.frontmatter?.date}</p>
+      <p>Posted: {data.mdx?.frontmatter?.date}</p>
+      {image ? (
+        <GatsbyImage
+        image={image}
+        alt={data.mdx?.frontmatter?.hero_image_alt ?? ''}
+        />
+      ): null }
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx?.frontmatter?.hero_image_credit_link ?? undefined}>
+          {data.mdx?.frontmatter?.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   )
@@ -18,11 +34,30 @@ export const query = graphql`
     mdx(id: {eq: $id}) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
 `
+
+// export const query = graphql`
+//   query BlogPost ($id: String) {
+//     mdx(id: {eq: $id}) {
+//       frontmatter {
+//         title
+//         date(formatString: "MMMM D, YYYY")
+//       }
+//     }
+//   }
+// `
 
 export const Head = ({ data }: PageProps<Queries.BlogPostQuery>) => <Seo title={data.mdx?.frontmatter?.title} />
 
